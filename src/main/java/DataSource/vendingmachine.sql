7c3eb8d0b60a1af69fb864e086a8ed8000f5c0a0
unknown <maingocdoan1809>
2023-07-13 03:45:02 +0700

    .

--- src/main/java/DataSource/vendingmachine.sql
+++ src/main/java/DataSource/vendingmachine.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2023 at 01:28 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Jul 12, 2023 at 10:44 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -65,12 +65,39 @@ INSERT INTO `banks` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cash`
+--
+
+CREATE TABLE `cash` (
+  `value` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  `name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cash`
+--
+
+INSERT INTO `cash` (`value`, `count`, `name`) VALUES
+(1000, 17, 'Một nghìn đồng'),
+(2000, 15, 'Hai nghìn đồng'),
+(5000, 18, 'Năm nghìn đồng'),
+(10000, 13, 'Mười nghìn đồng'),
+(20000, 10, 'Hai mươi nghìn đồng'),
+(50000, 11, 'Năm mươi nghìn đồng'),
+(100000, 10, 'Một trăm nghìn đồng'),
+(200000, 11, 'Hai trăm nghìn đồng'),
+(500000, 10, 'Năm trăm nghìn đồng');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `history`
 --
 
 CREATE TABLE `history` (
   `id` int(11) NOT NULL,
-  `user` varchar(100) NOT NULL,
+  `user` varchar(100) DEFAULT NULL,
   `product` int(11) NOT NULL,
   `time` date NOT NULL DEFAULT current_timestamp(),
   `price` float NOT NULL
@@ -81,7 +108,14 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`id`, `user`, `product`, `time`, `price`) VALUES
-(2, 'doandeptrai', 1, '2023-07-03', 10000);
+(2, 'doandeptrai', 1, '2023-07-03', 10000),
+(4, 'public', 7, '2023-07-12', 30000),
+(5, 'public', 2, '2023-07-12', 11000),
+(6, 'public', 7, '2023-07-12', 30000),
+(7, 'public', 2, '2023-07-12', 11000),
+(8, 'public', 2, '2023-07-12', 11000),
+(9, 'public', 7, '2023-07-12', 30000),
+(10, 'public', 2, '2023-07-12', 11000);
 
 -- --------------------------------------------------------
 
@@ -103,11 +137,11 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `price`, `remain`, `image`) VALUES
 (1, 'Coca Cola(320ml)', 10000, 10, NULL),
-(2, 'Pepsi(320ml)', 11000, 20, NULL),
+(2, 'Pepsi(320ml)', 11000, 16, NULL),
 (3, 'Sting (320ml)', 10500, 20, NULL),
 (4, 'Milk Coffee', 15000, 23, NULL),
 (5, 'Sprite', 12500, 9, NULL),
-(7, 'XX', 30000, 20, NULL),
+(7, 'XX', 30000, 17, NULL),
 (8, 'YY', 30, 30, NULL),
 (9, 'ZZ', 30000, 30, NULL),
 (10, 'ZKS', 10, 23, NULL);
@@ -152,6 +186,12 @@ ALTER TABLE `banks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cash`
+--
+ALTER TABLE `cash`
+  ADD PRIMARY KEY (`value`);
+
+--
 -- Indexes for table `history`
 --
 ALTER TABLE `history`
@@ -187,7 +227,7 @@ ALTER TABLE `banks`
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -209,7 +249,6 @@ ALTER TABLE `bankaccounts`
 -- Constraints for table `history`
 --
 ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`username`),
   ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`);
 
 --
